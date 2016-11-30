e621d2ef21b0e46dacb4446fe90d71e6e1063342
sonar.soni@gmail.com <sonar.soni@gmail.com>
2016-11-30 15:53:38 +0530

    disable email notification for now.

--- parakh-new/include/dbmodule.php
+++ parakh-new/include/dbmodule.php
@@ -361,7 +361,7 @@ class dbmodule {
      */
     function send_notification($email_data) {
         require_once 'notifications.php';
-        send_mail($email_data);
+        //send_mail($email_data);
     }
 
 //end of fun
