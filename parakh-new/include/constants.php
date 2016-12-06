cd44a41b1accc4fef22a43994de466b091d76409
namrata singh <namrata.singh@infobeans.com>
2016-12-06 20:36:26 +0530

    home page fixes

--- parakh-new/include/constants.php
+++ parakh-new/include/constants.php
@@ -3,7 +3,7 @@
 if ($_SERVER['HTTP_HOST'] == 'dev.parakh.com') {
     //Constants to connect with the local database
     define('DB_USERNAME', 'root');
-    define('DB_PASSWORD', 'root');
+    define('DB_PASSWORD', '');
     define('DB_HOST', 'localhost');
     define('DB_NAME', 'parakh-local');
     define('SALT', 'parakh-revamp-local-key-2016');
