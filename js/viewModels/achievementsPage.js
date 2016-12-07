54f562feb7d62667e773b60cabb066328402f4a0
shivangi-infobeans <shivangi.verma@infobeans.com>
2016-12-07 20:51:56 +0530

    achievements page design with responsive design

---
+++ js/viewModels/achievementsPage.js
@@ -0,0 +1,47 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * achievementsPage module
+ */
+define(['ojs/ojcore', 'knockout', 'jquery', 'ojs/ojknockout', 'ojs/ojbutton', 'ojs/ojmodel', 'ojs/ojinputtext', 'ojs/ojtabs', 'ojs/ojconveyorbelt' , 'ojs/ojdialog','ojs/ojknockout', 'promise', 'ojs/ojlistview'
+], function (oj, ko) {
+    /**
+     * The view model for the main content view template
+     */
+    function achievementsPageContentViewModel(person) {
+        var self = this;
+         var lgQuery = oj.ResponsiveUtils.getFrameworkQuery(
+                            oj.ResponsiveUtils.FRAMEWORK_QUERY_KEY.LG_UP);
+
+    self.large = oj.ResponsiveKnockoutUtils.createMediaQueryObservable('(min-width: 767px)');
+
+self.itemOnly = function(context)
+                {
+                    return context['leaf'];
+                }
+        
+       setTimeout ( function(){
+            $('a[href*="#"]:not([href="#"])').click(function() {
+  if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+    var target = $(this.hash);
+    target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+    if (target.length) {
+      $('html, body').animate({
+        scrollTop: target.offset().top
+      }, 1000);
+      return false;
+    }
+  }
+});  
+       },
+       500 );
+        
+    
+    }
+    
+    return achievementsPageContentViewModel;
+});
