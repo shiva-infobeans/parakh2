0eb307a11d0981e9c4ee36d0bd68027204aa46c8
anand-pandey <anand.pandey@infobeans.com>
2016-12-15 10:31:03 +0530

    add login.js and login.html files

---
+++ js/viewModels/login.js
@@ -0,0 +1,45 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * login module
+ */
+define(['ojs/ojcore', 'knockout', 'jquery', 'ojs/ojmodel', 'ojs/ojknockout', 'ojs/ojbutton', 'ojs/ojtabs', 'ojs/ojfilmstrip', 'ojs/ojpagingcontrol'
+], function (oj, ko, $) {
+    
+    function loginContentViewModel(){
+    	var self = this;
+    	self.video_image = ko.observable("");
+    	self.video_image("../../images/video.jpg");
+    	self.openVideo = function(){
+    		$("#modalVideo").ojDialog("open"); 
+		
+	        $.ajax({
+                headers: {secret: secret},
+                type: 'GET',
+                url: getParakhVideo,
+                data: {},
+                success: function(result){
+                    var data = JSON.parse(result)['data'];
+                    $('.oj-dialog-body').html(data.video);
+                }
+            });
+    	}
+
+    	var bg = $('#login-background').css('background-image');
+        bg = bg.replace('url(','').replace(')','').replace(/\"/gi, "");
+        if(bg==document.location.href+'images/parakh.jpg')
+        {
+        	
+        }else if(bg==document.location.href+'images/parakh2.jpg')
+        {
+        	
+        }
+    }
+
+    return loginContentViewModel;
+});
+
