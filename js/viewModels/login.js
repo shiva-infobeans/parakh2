05c7f47d54bf791fafd3fda1504e6d28d09b2db7
anand.pandey@infobeans.com <anand.pandey@infobeans.com>
2016-12-21 11:10:30 +0530

    update loader image name and place

--- js/viewModels/login.js
+++ js/viewModels/login.js
@@ -16,9 +16,6 @@ define(['ojs/ojcore', 'knockout', 'jquery', 'ojs/ojmodel', 'ojs/ojknockout', 'oj
     	self.video_image("../../images/video.jpg");
     	self.openVideo = function(){
     		$("#modalVideo").ojDialog("open"); 
-		    var video = '<video class="video" id="parakh_video" controls="" loop="" ><source type="video/ogg" src="Parakh_Teaser.mp4"><source type="video/mp4" src="Parakh_Teaser.mp4"><object  type="application/x-shockwave-flash" data="Parakh_Teaser.mp4" wmode="transparent"><param name="movie" value="Parakh_Teaser.mp4"><param name="wmode" value="transparent"><param name="autostart" value="false"></object></video>';
-            $('.oj-dialog-body').html(video);
-            
     	}
 
     	$(function(){
