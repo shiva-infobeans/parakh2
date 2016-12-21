46161032eeb5965015272036e1538a1ddfc28011
shivangi-infobeans <shivangi.verma@infobeans.com>
2016-12-21 12:08:27 +0530

    ranking page chnages

--- js/viewModels/topRanks.js
+++ js/viewModels/topRanks.js
@@ -16,7 +16,7 @@ define(['ojs/ojcore', 'knockout', 'jquery', 'ojs/ojknockout', 'ojs/ojchart', 'oj
         var initial = NAME.charAt(0) + NAME.charAt(NAME.lastIndexOf(" ") + 1);
         return initial;
     }
-    function Rankers1(x1, y1, z1, name1,id) {
+    function Rankers1(x1, y1, z1, name1, id) {
         var ranker = new Object();
         ranker.x = x1;
         ranker.y = parseInt(y1);
@@ -41,17 +41,19 @@ define(['ojs/ojcore', 'knockout', 'jquery', 'ojs/ojknockout', 'ojs/ojchart', 'oj
 
     function topRanksContentViewModel(person) {
         var self = this;
-        
-  self.selectedMenuItem = ko.observable();
 
-    self.menuItemSelect = function( event, ui ) {
-        console.log(event['currentTarget']);
-       
-    };
+        self.selectedMenuItem = ko.observable();
+
+        self.menuItemSelect = function (event, ui) {
+            console.log(event['currentTarget']);
+
+        };
 
         self.members = ko.observableArray([]);
         self.userId = ko.observable();
         this.myRank = ko.observable();
+        this.myRank1 = ko.observable();
+        this.myRank2 = ko.observable();
         this.totalMembers = ko.observable();
         self.loginUserId = ko.observable();
         self.name = ko.observable();
@@ -59,6 +61,8 @@ define(['ojs/ojcore', 'knockout', 'jquery', 'ojs/ojknockout', 'ojs/ojchart', 'oj
         var user = oj.Model.extend({
             url: getUserByEmail + person['email']
         });
+
+//        rank of logged in user overall
         var getDetails = new user();
         getDetails.fetch({
             headers: {secret: secret},
@@ -73,13 +77,71 @@ define(['ojs/ojcore', 'knockout', 'jquery', 'ojs/ojknockout', 'ojs/ojchart', 'oj
                     headers: {secret: secret},
                     success: function () {
                         self.myRank(memberRank['attributes']['data']['my_rank']);
-                        if(self.myRank() == "-"){
+                        console.log(memberRank['attributes']['data']['my_rank']);
+                        if (self.myRank() == "-") {
                             self.noRank("You have not yet received +1 rating.");
-                        }
-                        else{
+                        } else {
                             self.noRank("");
                         }
                         self.totalMembers(memberRank['attributes']['data']['total_user_count']);
+                    }
+                });
+            }
+        });
+
+        //        rank of logged in user in current month
+          var user1 = oj.Model.extend({
+            url: getUserByEmail + person['email']
+        });
+        var getMyMonthRank = new user1();
+        getMyMonthRank.fetch({
+            headers: {secret: secret},
+            success: function (result) {
+                self.loginUserId(result['attributes']['data']['id']);
+                var getRank1 = oj.Model.extend(
+                        {
+                            url: getMonthRank + self.loginUserId(),
+                        });
+                var memberRank1 = new getRank1();
+                memberRank1.fetch({
+                    headers: {secret: secret},
+                    success: function () {
+                        self.myRank1(memberRank1['attributes']['data']['my_rank']);
+                        console.log(memberRank1['attributes']['data']['my_rank']);
+                        if (self.myRank1() == "-") {
+                            self.noRank("You have not yet received +1 rating.");
+                        } else {
+                            self.noRank("");
+                        }
+                    }
+                });
+            }
+        });
+
+        //        rank of logged in user in past 90 days
+         var user2 = oj.Model.extend({
+            url: getUserByEmail + person['email']
+        });
+        var getMyQuarterRank = new user2();
+        getMyQuarterRank.fetch({
+            headers: {secret: secret},
+            success: function (result) {
+                self.loginUserId(result['attributes']['data']['id']);
+                var getRank2 = oj.Model.extend(
+                        {
+                            url: getQuarterRank + self.loginUserId(),
+                        });
+                var memberRank2 = new getRank2();
+                memberRank2.fetch({
+                    headers: {secret: secret},
+                    success: function () {
+                        self.myRank2(memberRank2['attributes']['data']['my_rank']);
+                         console.log(memberRank2['attributes']['data']['my_rank']);
+                        if (self.myRank2() == "-") {
+                            self.noRank("You have not yet received +1 rating.");
+                        } else {
+                            self.noRank("");
+                        }
                     }
                 });
             }
@@ -110,23 +172,23 @@ define(['ojs/ojcore', 'knockout', 'jquery', 'ojs/ojknockout', 'ojs/ojchart', 'oj
             success: function (res) {
                 var data1 = res['attributes']['data'];
                 for (var counter = 0; counter < data1.length; counter++) {
-                    self.data12.push(Rankers1((counter+1), data1[counter]['pluscount'], (90 - (counter * 5)), data1[counter]['google_name'],data1[counter]['user_id']));
+                    self.data12.push(Rankers1((counter + 1), data1[counter]['pluscount'], (90 - (counter * 5)), data1[counter]['google_name'], data1[counter]['user_id']));
                 }
                 for (var i = 0; i < self.data12().length; i++) {
                     self.bubbleSeries()[i].items.push({
                         x: self.data12()[i].x, y: self.data12()[i].y, z: self.data12()[i].z, label: self.data12()[i].label, labelPosition: 'auto',
                         shortDesc: "&lt;b&gt;" + self.data12()[i].Member + "&lt;/b&gt;" + "&lt;br/&gt;Total +1 ratings: " + self.data12()[i].y + "&lt;br/&gt;",
-                        user_id:self.data12()[i].userId
+                        user_id: self.data12()[i].userId
                     });
                 }
                 self.bubbleSeriesValue(self.bubbleSeries());
-               
+
             }
         });
-        
+
 //        month top ranks
-         self.data21 = ko.observableArray([]);
-        self.bubbleSeriesValue1 = ko.observableArray();
+        self.data21 = ko.observableArray([]);
+        self.bubbleSeriesValue = ko.observableArray();
         self.bubbleSeries = ko.observableArray([
             {name: "Series 1", displayInLegend: 'off', items: []},
             {name: "Series 2", displayInLegend: 'off', items: []},
@@ -139,8 +201,8 @@ define(['ojs/ojcore', 'knockout', 'jquery', 'ojs/ojknockout', 'ojs/ojchart', 'oj
             {name: "Series 9", displayInLegend: 'off', items: []},
             {name: "Series 10", displayInLegend: 'off', items: []}
         ]);
-        
-        
+
+
         var monthTopRanks = oj.Model.extend({
             url: getMonthTopRanks
                     //parse: parseTask
@@ -151,24 +213,24 @@ define(['ojs/ojcore', 'knockout', 'jquery', 'ojs/ojknockout', 'ojs/ojchart', 'oj
             success: function (res1) {
                 var data2 = res1['attributes']['data'];
                 for (var counter = 0; counter < data2.length; counter++) {
-                    self.data21.push(Rankers1((counter+1), data2[counter]['pluscount'], (90 - (counter * 5)), data2[counter]['google_name'],data2[counter]['user_id']));
+                    self.data21.push(Rankers1((counter + 1), data2[counter]['pluscount'], (90 - (counter * 5)), data2[counter]['google_name'], data2[counter]['user_id']));
                 }
                 for (var i = 0; i < self.data21().length; i++) {
                     self.bubbleSeries()[i].items.push({
                         x: self.data21()[i].x, y: self.data21()[i].y, z: self.data21()[i].z, label: self.data21()[i].label, labelPosition: 'auto',
                         shortDesc: "&lt;b&gt;" + self.data21()[i].Member + "&lt;/b&gt;" + "&lt;br/&gt;Total +1 ratings: " + self.data21()[i].y + "&lt;br/&gt;",
-                        user_id:self.data21()[i].userId
+                        user_id: self.data21()[i].userId
                     });
                 }
-                self.bubbleSeriesValue1(self.bubbleSeries());
-               
+                self.bubbleSeriesValue(self.bubbleSeries());
+
             }
         });
-        
+
 //        past 90 days top ranks
 
-         self.data31 = ko.observableArray([]);
-        self.bubbleSeriesValue2 = ko.observableArray();
+        self.data31 = ko.observableArray([]);
+        self.bubbleSeriesValue = ko.observableArray();
         self.bubbleSeries = ko.observableArray([
             {name: "Series 1", displayInLegend: 'off', items: []},
             {name: "Series 2", displayInLegend: 'off', items: []},
@@ -181,8 +243,8 @@ define(['ojs/ojcore', 'knockout', 'jquery', 'ojs/ojknockout', 'ojs/ojchart', 'oj
             {name: "Series 9", displayInLegend: 'off', items: []},
             {name: "Series 10", displayInLegend: 'off', items: []}
         ]);
-        
-        
+
+
         var quarterTopRanks = oj.Model.extend({
             url: getQuarterTopRanks
                     //parse: parseTask
@@ -193,29 +255,29 @@ define(['ojs/ojcore', 'knockout', 'jquery', 'ojs/ojknockout', 'ojs/ojchart', 'oj
             success: function (res1) {
                 var data3 = res1['attributes']['data'];
                 for (var counter = 0; counter < data3.length; counter++) {
-                    self.data31.push(Rankers1((counter+1), data3[counter]['pluscount'], (90 - (counter * 5)), data3[counter]['google_name'],data3[counter]['user_id']));
+                    self.data31.push(Rankers1((counter + 1), data3[counter]['pluscount'], (90 - (counter * 5)), data3[counter]['google_name'], data3[counter]['user_id']));
                 }
                 for (var i = 0; i < self.data31().length; i++) {
                     self.bubbleSeries()[i].items.push({
                         x: self.data31()[i].x, y: self.data31()[i].y, z: self.data31()[i].z, label: self.data31()[i].label, labelPosition: 'auto',
                         shortDesc: "&lt;b&gt;" + self.data31()[i].Member + "&lt;/b&gt;" + "&lt;br/&gt;Total +1 ratings: " + self.data31()[i].y + "&lt;br/&gt;",
-                        user_id:self.data31()[i].userId
+                        user_id: self.data31()[i].userId
                     });
                 }
-                self.bubbleSeriesValue2(self.bubbleSeries());
-                
+                self.bubbleSeriesValue(self.bubbleSeries());
+
             }
         });
-        
-        
-        self.chartOptionChange =  function(event, ui) {  
-          if (ui['option'] == 'selection') {
-              var userId = ui['optionMetadata']['selectionData'][0]['data']['user_id'];
-              window.location = "memberProfile.html?id=" + userId;
-          }
+
+
+        self.chartOptionChange = function (event, ui) {
+            if (ui['option'] == 'selection') {
+                var userId = ui['optionMetadata']['selectionData'][0]['data']['user_id'];
+                window.location = "memberProfile.html?id=" + userId;
+            }
         }
-        
-        
+
+
         var bubbleGroups = ["MEMBER"];
         this.bubbleGroupsValue = ko.observableArray(bubbleGroups);
         /* chart axes */
@@ -245,7 +307,7 @@ define(['ojs/ojcore', 'knockout', 'jquery', 'ojs/ojknockout', 'ojs/ojchart', 'oj
                     lineStyle: self.xMajorTickStyle1()[0]
                 },
                 tickLabel: {
-                    style : 'color:#fff'
+                    style: 'color:#fff'
                 }
             };
         });
@@ -263,7 +325,7 @@ define(['ojs/ojcore', 'knockout', 'jquery', 'ojs/ojknockout', 'ojs/ojchart', 'oj
                 },
                 tickLabel: {
                     position: self.yTickLabelPosition1()[0],
-                    style : 'color:#fff'
+                    style: 'color:#fff'
                 }
             };
         });
