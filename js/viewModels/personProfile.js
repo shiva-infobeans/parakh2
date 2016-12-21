e717e90b6791c41826da9b53dacb3edd130314b8
anand.pandey@infobeans.com <anand.pandey@infobeans.com>
2016-12-21 15:52:05 +0530

    solve update past project issue

--- js/viewModels/personProfile.js
+++ js/viewModels/personProfile.js
@@ -360,7 +360,7 @@ define(['ojs/ojcore', 'knockout', 'jquery', 'ojs/ojtabs', 'ojs/ojconveyorbelt', 
                             {
                                 self.successful("Profile updated successfully!");
                                 $('.sucessMsg').show();
-                                self.allRevert();
+                                self.revertAfterEdit();
                             }
                             setTimeout(function () {
                                 $('.sucessMsg').hide();
@@ -743,6 +743,26 @@ define(['ojs/ojcore', 'knockout', 'jquery', 'ojs/ojtabs', 'ojs/ojconveyorbelt', 
                     {
                         self.myNumber("+91-" + self.temporaryNumber());
                     }
+                }
+
+                self.revertAfterEdit = function(){
+                    self.moberror("");
+                    $('#designation-text').removeClass('hide');
+                    $('#designation-div').addClass('hide');
+                    $('#location-text').removeClass('hide');
+                    $('#location-div').addClass('hide');
+                    $('#skills-text').removeClass('hide');
+                    $('#skills').addClass('hide');
+                    $('#associate-text').removeClass('hide');
+                    $('#associate-div').addClass('hide');
+                    $('#primary-project-text').removeClass('hide');
+                    $('#primary-project-div').addClass('hide');
+                    $('#projects-text').removeClass('hide');
+                    $('#projects-div').addClass('hide');
+                    $('#interest-text').removeClass('hide');
+                    $('#interest-div').addClass('hide');
+                    $('#number-text').removeClass('hide');
+                    $('#editNumberBox').addClass('hide');
                 }
 
                 self.allRevert = function () {
